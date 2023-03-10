9d6b7330f32541d8b7ea850b3fd5e35b95aa0e4a
user <user@DESKTOP-2PGRMF4>
2023-03-10 10:39:48 +0900

    1. First Commit

---
+++ och01Git/src/main/java/och01/GuGu.java
@@ -0,0 +1,55 @@
+package och01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GuGu
+ */
+// @WebServlet("/GuGu")	// WebServlet에 들어가는 이름이 서버의 주소명.
+public class GuGu extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GuGu() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. Parameter-> num
+		// 2. 구구단 출력
+		int num = Integer.parseInt(request.getParameter("num"));
+								// request하는 속성의 name을 파라미터로 받는다.
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.print("<html><body>");
+		out.print("<h1>구구단 " + num + "단</h1>");
+		
+		for (int i=1; i<=9; i++) {
+			out.printf("%d * %d = %d<br>", num, i, (num*i));
+		}
+		out.print("</body></html>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
